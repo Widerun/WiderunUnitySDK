52f26a6369e596dc3f9398598a600993e702c78b
unknown <tiziano@piccardi.me>
2014-10-11 23:39:47 +0200

    wizard draft

---
+++ EditorWizard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+public class EditorWizard : ScriptableWizard {
+	public float seed = 12345f;
+	public string heightsFile = "";
+	TextAsset txt = new TextAsset();
+	[Range(-1, 1)]
+	public float leftHeight = 0;
+	[Range(-1, 1)]
+	public float rightHeight = 0;
+
+
+	[MenuItem ("Widerun/Create Terrain")]
+	static void CreateWizard () {
+		ScriptableWizard.DisplayWizard<EditorWizard>("Create Widerun terrain", "Create");
+	}
+	void OnWizardCreate () {
+		
+	}  
+	void OnWizardUpdate () {
+		helpString = "Please set the terrain details!";
+	}   
+
+}
